4bc9fc152e14fef98477b6acf1671f6ddb26c809
zw <az4600242885@qq.com>
2018-07-08 16:58:04 +0800

    底部导航栏切换完成

--- App/iTalker/app/src/main/java/com/imist/italker/push/MainActivity.java
+++ App/iTalker/app/src/main/java/com/imist/italker/push/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends Activity implements BottomNavigationView.OnNav
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         mTittle.setText(item.getTitle());
-        return false;
+        return true;
     }
 
 
