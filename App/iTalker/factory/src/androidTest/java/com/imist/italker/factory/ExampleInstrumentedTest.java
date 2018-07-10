e62b667dfde178a3004aba7992a96c222a0a6814
zw <az4600242885@qq.com>
2018-07-10 09:47:47 +0800

    重新导入factory bao

--- App/iTalker/factory/src/androidTest/java/com/imist/italker/factory/ExampleInstrumentedTest.java
+++ App/iTalker/factory/src/androidTest/java/com/imist/italker/factory/ExampleInstrumentedTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -21,6 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+
         assertEquals("com.imist.italker.factory.test", appContext.getPackageName());
     }
 }
