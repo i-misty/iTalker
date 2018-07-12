08838f8f22aeb9e42d7fdf0e768409cfa2e575bb
zw <az4600242885@qq.com>
2018-07-12 09:51:27 +0800

    添加工具类，修改数据库名字

--- Web/iTalker/src/main/java/net/imist/web/italker/push/Application.java
+++ Web/iTalker/src/main/java/net/imist/web/italker/push/Application.java
@@ -1,6 +1,7 @@
 package net.imist.web.italker.push;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.imist.web.italker.push.provider.GsonProvider;
 import net.imist.web.italker.push.service.AccountService;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -15,7 +16,8 @@ public class Application extends ResourceConfig {
         //packages("net.imist.web.italker.push.service");
         packages(AccountService.class.getPackage().getName());
         //注册json解析器
-        register(JacksonJsonProvider.class);
+        //register(JacksonJsonProvider.class);
+        register(GsonProvider.class);
         //注册日志打印输出
         register(Logger.class);
     }
